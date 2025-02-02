d40cf42d597fba0ca78c1b9f2777cc27cb2d7768
Hicham Mallah <mallah.hicham@gmail.com>
2025-02-02 12:14:55 +0200

    more testing

--- ldm.s
+++ ldm.s
@@ -3,8 +3,13 @@
 
 _start:
 	ldr R0, =values
-	ldm R0, {R1-R8}
+	ldr R1, =values2
+	ldm R0, {R2-R9}
+	ldm R1, {R10-R12}
+	stm R0, {R10-R12}
 
 .section .data
 values:
 	.word 1,2,3,4,5,6,7,8
+values2: 
+	.word 9,10,11
